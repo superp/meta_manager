ccc578309db86c0447598e214d1869c636eb649e
superp <superp1987@gmail.com>
2012-05-28 11:14:18 +0300

    fix ActiveSupport::Concern deprecation

--- lib/meta_manager/taggable.rb
+++ lib/meta_manager/taggable.rb
@@ -8,45 +8,41 @@ module MetaManager
       accepts_nested_attributes_for :meta_tags, :reject_if => :all_blank, :allow_destroy => true
     end
     
-    module InstanceMethods
+    # Find meta tag by name or build new record
+    def find_or_build_meta_tag(attr_name)
+      key = normalize_meta_tag_name(attr_name)
+      self.meta_tags.where(:name => key).first || self.meta_tags.build(:name => key)
+    end
     
-      # Find meta tag by name or build new record
-      def find_or_build_meta_tag(attr_name)
-        key = normalize_meta_tag_name(attr_name)
-        self.meta_tags.where(:name => key).first || self.meta_tags.build(:name => key)
-      end
+    # Save meta tags records into one hash
+    def meta_tag(attr_name)
+      key = normalize_meta_tag_name(attr_name)
+      @meta_tag ||= {}
+      @meta_tag[key] ||= find_or_build_meta_tag(key)
+    end
+    
+    protected
+    
+    def normalize_meta_tag_name(value)
+      key = value.to_s.downcase.strip
+      key
+    end
+    
+    def method_missing(method, *args, &block)
+      key = method.to_s
       
-      # Save meta tags records into one hash
-      def meta_tag(attr_name)
-        key = normalize_meta_tag_name(attr_name)
-        @meta_tag ||= {}
-        @meta_tag[key] ||= find_or_build_meta_tag(key)
-      end
-      
-      protected
-      
-        def normalize_meta_tag_name(value)
-          key = value.to_s.downcase.strip
-          key
-        end
+      if key =~ /^tag_/
+        attr_name = key.gsub(/^tag_/, '')
         
-        def method_missing(method, *args, &block)
-          key = method.to_s
-          
-          if key =~ /^tag_/
-            attr_name = key.gsub(/^tag_/, '')
-            
-            if key =~ /=$/
-              record = meta_tag(attr_name.chop)
-              record.content = args.first
-            else 
-              meta_tag(attr_name).try(:content)
-            end
-          else
-            super
-          end
+        if key =~ /=$/
+          record = meta_tag(attr_name.chop)
+          record.content = args.first
+        else 
+          meta_tag(attr_name).try(:content)
         end
-      
+      else
+        super
+      end
     end
   end
 end
